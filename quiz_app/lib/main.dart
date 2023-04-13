8c1b342219adf8e94a123befaf84e0dfcc2e8b11
teoColomberotto <teocolomberotto97@gmail.com>
2023-04-13 14:59:38 +0200

    firebase setup

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,9 +1,35 @@
 import 'package:flutter/material.dart';
 
-//import 'package:firebase_core/firebase_core.dart';
-//import 'package:flutter_bloc/flutter_bloc.dart';
-import 'app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'app.dart';
+import 'app_bloc_observer.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp()
+      // MultiBlocProvider(
+      //   providers: [
+      //     BlocProvider(
+      //       create: (context) =>
+      //           AuthenticationBloc(AuthenticationRepositoryImpl())
+      //             ..add(AuthenticationStarted()),
+      //     ),
+      //     BlocProvider(
+      //       create: (context) => FormBloc(
+      //           AuthenticationRepositoryImpl(), DatabaseRepositoryImpl()),
+      //     ),
+      //     BlocProvider(
+      //       create: (context) => DatabaseBloc(DatabaseRepositoryImpl()),
+      //     )
+      //   ],
+      //   child: const MyApp(),
+      // ),
+      );
+  Bloc.observer = AppBlocObserver();
 }
