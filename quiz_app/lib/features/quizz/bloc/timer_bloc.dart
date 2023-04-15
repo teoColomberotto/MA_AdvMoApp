8317a34020be460addc9684ad3b1e71d987850dd
teoColomberotto <teocolomberotto97@gmail.com>
2023-04-15 20:35:05 +0200

    timer bloc implemented

--- quiz_app/lib/features/quizz/bloc/timer_bloc.dart
+++ quiz_app/lib/features/quizz/bloc/timer_bloc.dart
@@ -2,43 +2,69 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/scheduler.dart';
+
+import '../models/ticker_model.dart';
 
 part 'timer_event.dart';
 part 'timer_state.dart';
 
 class TimerBloc extends Bloc<TimerEvent, TimerState> {
-  late final Ticker _ticker;
+  late final int _duration;
+  late final MyTicker _ticker;
   late final StreamSubscription<int> tickerSubscription;
 
-  TimerBloc({required duration, required Ticker ticker})
+  TimerBloc({required duration, required MyTicker ticker})
       : super(Ready(duration)) {
     _ticker = ticker;
+    _duration = duration;
 
     on<TimerEvent>((event, emit) {
       if (event is Start) {
-        _mapStartToState(event);
+        _mapStartToState(event, emit);
       } else if (event is Pause) {
-        _mapPauseToState(event);
+        _mapPauseToState(event, emit);
       } else if (event is Resume) {
-        _mapResumeToState(event);
+        _mapResumeToState(event, emit);
       } else if (event is Reset) {
-        _mapResetToState(event);
+        _mapResetToState(event, emit);
       } else if (event is Tick) {
-        _mapTickToState(event);
+        _mapTickToState(event, emit);
       }
     });
   }
 
-  void _mapStartToState(Start event) {}
+  void _mapStartToState(Start event, emit) {
+    emit(Running(event.duration));
+    tickerSubscription.cancel();
+    tickerSubscription = _ticker.tick(ticks: event.duration).listen(
+      (duration) {
+        add(Tick(duration: duration));
+      },
+    );
+  }
 
-  void _mapPauseToState(Pause event) {}
+  void _mapPauseToState(Pause event, emit) {
+    if (state is Running) {
+      tickerSubscription.pause();
+      emit(Paused(state.duration));
+    }
+  }
 
-  void _mapResumeToState(Resume event) {}
+  void _mapResumeToState(Resume event, emit) {
+    if (state is Paused) {
+      tickerSubscription.resume();
+      emit(Running(state.duration));
+    }
+  }
 
-  void _mapResetToState(Reset event) {}
+  void _mapResetToState(Reset event, emit) {
+    tickerSubscription.cancel();
+    emit(Ready(_duration));
+  }
 
-  void _mapTickToState(Tick event) {}
+  void _mapTickToState(Tick event, emit) {
+    event.duration > 0 ? emit(Running(event.duration)) : emit(const Finished());
+  }
 
   @override
   Future<void> close() {
--- quiz_app/lib/features/quizz/models/ticker_model.dart
+++ quiz_app/lib/features/quizz/models/ticker_model.dart
@@ -1,4 +1,4 @@
-class Ticker {
+class MyTicker {
   Stream<int> tick({required int ticks}) {
     return Stream.periodic(const Duration(seconds: 1), (x) => ticks - x - 1)
         .take(ticks);
