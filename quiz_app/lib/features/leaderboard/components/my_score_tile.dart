b234f4dfe7c133e9776fde36168a433b51a0f32e
teoColomberotto <teocolomberotto97@gmail.com>
2023-04-30 01:16:10 +0200

    leaderboard date formatted

--- quiz_app/lib/features/leaderboard/components/my_score_tile.dart
+++ quiz_app/lib/features/leaderboard/components/my_score_tile.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import '../../database/models/score_model.dart';
 
 class MyScoreTile extends StatelessWidget {
   final Score score;
   final int index;
-
-  const MyScoreTile({Key? key, required this.score, required this.index})
-      : super(key: key);
+  late final String formattedData;
+  MyScoreTile({Key? key, required this.score, required this.index})
+      : super(key: key) {
+    formattedData = DateFormat('d MMMM y').format(score.timestamp);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +20,7 @@ class MyScoreTile extends StatelessWidget {
           child: Text(score.score.toString()),
         ),
         title: Text(score.name),
-        subtitle: Text(score.timestamp.toString()),
+        subtitle: Text(formattedData),
         trailing: Text('#$index'),
       ),
     );
